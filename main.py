1fb81caf63aa416056442ee6b051da01eec0b872
Dylan Moore <hosnappp@gmail.com>
2016-08-11 05:58:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,37 @@ import os
 from subprocess import Popen, PIPE, STDOUT, call
 
 print("\nHey there! Sit tight, this isn't going to hurt one bit.\n")
-
-  # Install our beloved Vagrant VM manager/provision  
-call("sudo apt-get install vagrant", shell=True)
-
-  # Create folder & cd
 print("Setting up Vagrant environment...\n")
+	
 if not os.path.exists("Vagrant"):
 	os.makedirs("Vagrant")
 os.chdir("Vagrant")
-
-  # Download Ubuntu box from Vagrant site
 print("Downloading Ubuntu for the VM...\n")
-p = Popen("vagrant box add ubuntu/trusty64", stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
+p = Popen("vagrant box add ubuntu/trusty32", stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
 print("\nUbuntu 14.04 LTS (Trusty Tahr) downloaded...\n")
-
-  # Select "2.) virtual box" by sending binary encoded TTY stdin
 stdout = p.communicate(input=b'2')[0]
 print("\nSetting up VirtualBox components...\n")
 print(stdout.decode())
-
-  # Install VirtualBox if it's not present on the system
 print("\nSuccessfully selected VirtualBox for setup...\n")
 print("\nInstalling VirtualBox...\n")
 call("sudo apt-get install virtualbox", shell=True)
-
-  # Initialize Vagrant & system access
-call("vagrant init ubuntu/trusty64", shell=True)
+call("sudo apt-get install software-properties-common", shell=True)
+p = Popen("sudo apt-add-repository ppa:ansible/ansible", stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
+stdout = p.communicate(input=b'y')[0]
+print(stdout.decode('utf-8'))
+call("sudo apt-get update", shell=True)
+call("sudo apt-get install ansible", shell=True)
+call("vagrant init ubuntu/trusty32 --minimal", shell=True)
+f = open("Vagrantfile", "w")
+f.write("Vagrant.configure(\"2\") do |config|\n")
+f.write("  config.vm.box = \"ubuntu/trusty32\"\n")
+f.write("   config.vm.network \"forwarded_port\", guest: 8088, host: 8088\n")
+f.write("  config.vm.provision \"ansible\" do |ansible|\n")
+f.write("    ansible.playbook = \"playbook.yml\"\n")
+f.write("   end\n")
+f.write("end")
+f.close()
+call("sudo mv ../playbook.yml .", shell=True)
 call("vagrant up", shell=True)
 call("vagrant ssh", shell=True)
+
